ba2d41c02709a7f25aaa16e1c9eadce6a5545fe2
allandereal <ahullan@gmail.com>
2021-07-15 10:37:48 +0300

    add column and card relationship

--- app/Models/BoardColumn.php
+++ app/Models/BoardColumn.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class BoardColumn extends Model
 {
     use HasFactory;
+
+    public function cards()
+    {
+        return $this->hasMany(BoardCard::class);
+    }
 }
